18c1d95479b4d05ca9548fdee25aeb4593b5b97e
ashwiniahire888 <89189805+ashwiniahire888@users.noreply.github.com>
2023-10-27 19:39:12 +0530

    Add files via upload

--- ATM/src/atmsimulatorsystem\assistant/Database/DatabaseHandler.java
+++ ATM/src/atmsimulatorsystem\assistant/Database/DatabaseHandler.java
@@ -1,1 +1,125 @@
+package atmsimulatorsystem.assistant.Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author ABHIJEET KARMAKER <C0720286>, NARESH GUNIMANIKULA <C0719672>, PRIYANKA MODI <C0717925>
+ */
+public class DatabaseHandler {
+
+    //Declare JDBC Driver
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+    //Connection
+    public static Connection conn = null;
+    private static Statement stmt = null;
+    private static final String connStr = "jdbc:mysql://localhost:3306/atmsystem?zeroDateTimeBehavior=convertToNull";
+
+    /**
+     * 
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    public static void dbConnect() throws SQLException, ClassNotFoundException {
+        //Setting Oracle JDBC Driver
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySql JDBC Driver?");
+            e.printStackTrace();
+            throw e;
+        }
+
+        System.out.println("MySql JDBC Driver Registered!");
+
+        //Establish the Oracle Connection using Connection String
+        try {
+            conn = DriverManager.getConnection(connStr, "root", "root");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console" + e);
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    /**
+     * 
+     * @throws SQLException 
+     */
+    //Close Connection
+    public static void dbDisconnect() throws SQLException {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    /**
+     * 
+     * @param queryStmt
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    //DB Execute Query Operation
+    public static ResultSet dbExecuteQuery(String queryStmt) throws SQLException, ClassNotFoundException {
+        //Declare statement, resultSet as null
+        Statement stmt = null;
+        ResultSet resultSet = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            System.out.println("Select statement: " + queryStmt + "\n");
+
+            //Create statement
+            stmt = conn.createStatement();
+
+            //Execute select (query) operation
+            resultSet = stmt.executeQuery(queryStmt);
+
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeQuery operation : " + e);
+            throw e;
+        } finally {
+        }
+        return resultSet;
+    }
+
+    /**
+     * 
+     * @param sqlStmt
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    //DB Execute Update (For Update/Insert/Delete) Operation
+    public static void dbExecuteUpdate(String sqlStmt) throws SQLException, ClassNotFoundException {
+        //Declare statement as null
+        Statement stmt = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            //Create Statement
+            stmt = conn.createStatement();
+            //Run executeUpdate operation with given sql statement
+            stmt.executeUpdate(sqlStmt);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeUpdate operation : " + e);
+            throw e;
+        } finally {
+            if (stmt != null) {
+                //Close statement
+                stmt.close();
+            }
+            //Close connection
+            dbDisconnect();
+        }
+    }
+}
